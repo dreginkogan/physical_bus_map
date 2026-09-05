2e490a8670f0ae823f26f1a2a28ec292264e2ffa
danya <dreginkogan@gmail.com>
2026-09-05 02:09:56 -0400

    worked on coords_to_led.py

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from display import display_loop
 from prt import prt_loop
 from state import State, StateManager
 
+from coord_to_led import coords_to_led
+
+print(coords_to_led("88", "OUTBOUND", 5.6, 5.6))
+
 
 if __name__ == "__main__":
     # (streets, bus_routes) = parse_data_files()
