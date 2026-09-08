ef7eeb0c79604540f7a75ab4ce1b05175ab87cc8
danya <dreginkogan@gmail.com>
2026-09-08 00:25:51 -0400

    hopefully the leds light up

--- display.py
+++ display.py
@@ -13,6 +13,11 @@ BRIGHTNESS = 0.6
 # auto_write = False means that changes won't show until pixels.show() is called
 pixels = neopixel.NeoPixel(LED_PIN, LED_COUNT, brightness=BRIGHTNESS, auto_write=False)
 
+def hex_to_rgb(value):
+    value = value.lstrip('#')
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
 def update_display(state: State):
 
     vehicles_list = state.busses
@@ -28,8 +33,11 @@ def update_display(state: State):
 
         print(f"Need to lights leds {leds_to_light} the color {color}")
 
+        for led in leds_to_light:
+            pixels[led] = hex_to_rgb(color)
 
-    # TODO
+    pixels.show()
+
     pass
 
 def display_loop(state_mgr: StateManager):
