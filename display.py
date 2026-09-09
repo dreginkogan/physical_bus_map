f032cfd4c2376c1183dc3613fd670a8c524e211d
danya <dreginkogan@gmail.com>
2026-09-09 10:28:20 -0400

    made leds clear (also it works now)

--- display.py
+++ display.py
@@ -19,7 +19,7 @@ def hex_to_rgb(value):
     return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
 def update_display(state: State):
-    # pixels.fill((0, 0, 0)) #off for now
+    pixels.fill((0, 0, 0)) #off for now
 
     vehicles_list = state.busses
 
