43b97a43fc31c843773f6817b1c9bb3c74658c5e
danya <dreginkogan@gmail.com>
2026-09-08 23:53:34 -0400

    hopefully coord to led issue is fixed?

--- coord_to_led.py
+++ coord_to_led.py
@@ -10,8 +10,7 @@ with open('routes.json') as f:
     routes_dict = json.load(f)
 
 def distance(a_coord, b_coord):
-    #technically returns dist squared but oh well
-    return (a_coord[0] - b_coord[0])**2 + (a_coord[1] - b_coord[1])
+    return (a_coord[0] - b_coord[0])**2 + (a_coord[1] - b_coord[1])**2
 
 def coords_to_led(rt: str, dir: str, lat: float, lon: float):
     #TODO: make it so something like 3 or 5 or whatever adjacent leds can be returned,
@@ -20,47 +19,38 @@ def coords_to_led(rt: str, dir: str, lat: float, lon: float):
     segments_list = routes_dict["ROUTES"][rt][dir]
     color = routes_dict["ROUTES"][rt]["COLOR"]
 
-    led_list = []
-    coords_list = [] #interpolate values based on node coords
+    coord_led_pairs = []
     leds_to_light = []
 
     for segment in segments_list:
         segment_leds = segments_dict[segment]["led_ids"]
         segment_length = len(segment_leds)
 
-        segment_coords = []
+        start_coords = tuple(nodes_dict[segments_dict[segment]["start_node"]]["coordinates"])
+        end_coords = tuple(nodes_dict[segments_dict[segment]["end_node"]]["coordinates"])
 
-        start_coords = (nodes_dict[segments_dict[segment]["start_node"]]["coordinates"][0],
-                         nodes_dict[segments_dict[segment]["start_node"]]["coordinates"][1])
+        segment_coords = [start_coords]
 
-        end_coords = (nodes_dict[segments_dict[segment]["end_node"]]["coordinates"][0],
-                         nodes_dict[segments_dict[segment]["end_node"]]["coordinates"][1])
+        lat_interval = (end_coords[0] - start_coords[0]) / segment_length
+        lon_interval = (end_coords[1] - start_coords[1]) / segment_length
 
-        
-
-        # can do this with much fewer lines and better and more efficient practices, will deal with later
-
-        #TODO: make this inefficient mess better
-        segment_coords.append(start_coords)
-
-        lat_interval = (end_coords[0] - start_coords[0])/segment_length
-        lon_interval = (end_coords[1] - start_coords[1])/segment_length
-
-        for i in range(1, segment_length-1):
-            segment_coords.append((start_coords[0] + lat_interval*i, start_coords[1] + lon_interval*i))
+        for i in range(1, segment_length - 1):
+            segment_coords.append((start_coords[0] + lat_interval * i,
+                                    start_coords[1] + lon_interval * i))
 
         segment_coords.append(end_coords)
-        coords_list = list(set(coords_list + segment_coords))
-        led_list = list(set(led_list + segment_leds))
 
-    min_diff, res = float('inf'), None
-    for idx, coord in enumerate(coords_list):
-        if distance((lat, lon), coord) < min_diff:
-            min_diff = distance((lat, lon), coord)
-            res = idx
+        # zip coords to their LED ids together, then dedupe the pairs as a unit
+        segment_pairs = list(zip(segment_coords, segment_leds))
+        coord_led_pairs = list(dict.fromkeys(coord_led_pairs + segment_pairs))
 
-    nearest_led = led_list[res]
+    min_diff, nearest_led = float('inf'), None
+    for coord, led_id in coord_led_pairs:
+        d = distance((lat, lon), coord)
+        if d < min_diff:
+            min_diff = d
+            nearest_led = led_id
 
-    leds_to_light.append(nearest_led) # so i can expand the number of leds i return
+    leds_to_light.append(nearest_led)
 
     return leds_to_light, color
